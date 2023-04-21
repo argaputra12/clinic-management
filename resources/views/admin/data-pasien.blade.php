5d75f635e5daa32c543c1961b04fed6128830be3
argaputra12 <argaputra@student.uns.ac.id>
2023-04-22 01:17:17 +0700

    adding icons

--- resources/views/admin/data-pasien.blade.php
+++ resources/views/admin/data-pasien.blade.php
@@ -8,6 +8,11 @@
     </x-slot>
 
     <div class="p-6 overflow-hidden bg-white rounded-md shadow-md dark:bg-dark-eval-1 mb-4">
+        <div class="mb-6 mx-4 flex justify-end">
+            <a  class="px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-300 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-blue cursor-pointer">
+                Tambah Pasien
+            </a>
+        </div>
         <div class="flex font-semibold text-base justify-between border-b-2 pb-4">
             <div class="text-center w-[150px]">No Rekam Medis</div>
             <div class="text-center w-[150px]">Tanggal Kunjungan</div>
@@ -26,9 +31,15 @@
                 <div class="text-center w-[150px]">{{ $p->tanggal_lahir }}</div>
                 <div class="text-center w-[100px]">{{ $p->umur }}</div>
                 <div class="text-center w-[100px]">{{ $p->jenis_kelamin }}</div>
-                <div class="text-center w-[200px]">Aksi</div>
+                <div class="text-center w-[200px] flex justify-evenly items-center">
+                    <i class="fa-solid fa-circle-info fa-xl"></i>
+                    <i class="fa-solid fa-pen-to-square fa-xl"></i>
+                    <i class="fa-solid fa-trash fa-xl"></i>
+                </div>
             </div>
         @endforeach
     </div>
+
+
     <?php echo $pasien->render(); ?>
 </x-app-layout>

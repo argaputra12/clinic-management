254fb7a518c74c8fb017522a57b2160ddfa959c3
argaputra12 <argaputra@student.uns.ac.id>
2023-05-12 15:44:18 +0700

    update dashboard view

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,13 +1,50 @@
 <x-app-layout>
-    <x-slot name="header">
+  {{-- <x-slot name="header">
         <div class="flex flex-col gap-4 md:flex-row md:items-center md:justify-between">
             <h2 class="text-xl font-semibold leading-tight">
                 {{ __('Admin Page') }}
             </h2>
         </div>
-    </x-slot>
-
-    <div class="p-6 overflow-hidden bg-white rounded-md shadow-md dark:bg-dark-eval-1">
-        {{ __("You're logged in!")  }}
+    </x-slot> --}}
+  <div class="container min-h-screen flex justify-center items-center">
+    <div class="flex justify-center items-center w-1/2 flex-wrap">
+      <div class="p-4 overflow-hidden flex justify-center items-center dark:bg-dark-eval-1 basis-1/3">
+        <div class="bg-primary-cream  rounded-md shadow-md flex flex-col gap-4 justify-center items-center w-52 h-52">
+          <i class="text-center fa-4x fa-solid fa-stethoscope"></i>
+          <h2 class="font-semibold text-lg">Pengguna</h2>
+        </div>
+      </div>
+      <div class="p-4 overflow-hidden flex justify-center items-center dark:bg-dark-eval-1 basis-1/3">
+        <div class="bg-primary-cream rounded-md shadow-md flex flex-col gap-4 justify-center items-center w-52 h-52">
+          <i class="text-center fa-4x fa-solid fa-heart-pulse"></i>
+          <h2 class="font-semibold text-lg">Rekam Medis</h2>
+        </div>
+      </div>
+      <div class="p-4 overflow-hidden flex justify-center items-center dark:bg-dark-eval-1 basis-1/3 ">
+        <div class="bg-primary-cream rounded-md shadow-md flex flex-col gap-4 justify-center items-center w-52 h-52">
+          <i class="text-center fa-4x fa-solid fa-capsules"></i>
+          <h2 class="font-semibold text-lg">Resep Obat</h2>
+        </div>
+      </div>
+      <div class="p-4 overflow-hidden flex justify-center items-center dark:bg-dark-eval-1 basis-1/3 ">
+        <div class="bg-primary-cream rounded-md shadow-md flex flex-col gap-4 justify-center items-center w-52 h-52">
+          <i class="text-center fa-4x fa-solid fa-house-medical"></i>
+          <h2 class="font-semibold text-lg">Pasien</h2>
+        </div>
+      </div>
+      <div class="p-4 overflow-hidden flex justify-center items-center dark:bg-dark-eval-1 basis-1/3 ">
+        <div class="bg-primary-cream rounded-md shadow-md flex flex-col gap-4 justify-center items-center w-52 h-52">
+          <i class="text-center fa-4x fa-solid fa-file-invoice"></i>
+          <h2 class="font-semibold text-lg">Pembayaran</h2>
+        </div>
+      </div>
+      <div class="p-4 overflow-hidden flex justify-center items-center dark:bg-dark-eval-1 basis-1/3 ">
+        <div class="bg-primary-cream rounded-md shadow-md flex flex-col gap-4 justify-center items-center w-52 h-52">
+          <i class="text-center fa-4x fa-solid fa-file"></i>
+          <h2 class="font-semibold text-lg">Laporan</h2>
+        </div>
+      </div>
     </div>
+  </div>
+
 </x-app-layout>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -54,14 +54,14 @@
                 <x-navbar />
 
                 <!-- Page Heading -->
-                <header>
+                {{-- <header>
                     <div class="p-4 sm:p-6">
                         {{ $header }}
                     </div>
-                </header>
+                </header> --}}
 
                 <!-- Page Content -->
-                <main class="px-4 sm:px-6 flex-1">
+                <main class="min-h-screen bg-white flex-1">
                     {{ $slot }}
                 </main>
 
